3e9da40f7543c8b635abd81297f534558327ec65
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-08-21 19:10:33 +0530

    second commit

---
+++ A3.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class A3 {
+
+	public static void main(String[] args) {
+     ArrayList<String> al=new ArrayList<String>();
+		
+		al.add("Red");
+		al.add("Green");
+		al.add("Orange");
+		al.add("White");
+	    al.add("Black");
+	    
+	    System.out.println(al);
+	 
+	    al.add(0,"Pink");
+	  
+	    System.out.println("\noutput "+al);
+	}
+
+}
