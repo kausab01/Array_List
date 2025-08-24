25f390a79c7ad9b8ccc14942df2e8e387b8ba1ca
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-08-24 12:17:59 +0530

    third commit

---
+++ A4.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class A4 {
+
+	public static void main(String[] args) {
+		  ArrayList<String> al=new ArrayList<String>();
+			
+			al.add("Red");
+			al.add("Green");
+			al.add("Orange");
+			al.add("White");
+		    al.add("Black");
+		 
+		    System.out.println("First element : "+al.get(0));
+		    System.out.println("Second element : "+al.get(2));
+	}
+
+}
