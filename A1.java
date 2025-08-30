35e70fc39e438eac7894065de16e6f74b9f37f95
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-08-30 21:39:17 +0530

    Arraylist-1

---
+++ A1.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class A1 {
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> al=new ArrayList<String>();
+		
+		al.add("Red");
+		al.add("Green");
+		al.add("Orange");
+		al.add("White");
+		al.add("Black");
+		 
+		System.out.println(al);
+
+	}
+
+}
