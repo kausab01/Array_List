93b44c7d2a938fe5277fbca6eac4a170282b7895
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-09-10 18:32:36 +0530

    Arraylist-17

---
+++ A17.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class A17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  ArrayList<String> list1=new ArrayList<String>();
+			
+			list1.add("Red");
+			list1.add("Green");
+			list1.add("Black");
+			list1.add("White");
+		    list1.add("Pink");
+		    
+		    System.out.println("OG list : "+list1);
+		    list1.clear();
+		    
+		    System.out.println("\nAfter removing all element : "+list1);
+	}
+
+}
